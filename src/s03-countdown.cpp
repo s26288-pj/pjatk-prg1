1f12e341132841c88a8df7e1487fd4671df0e38b
s26288 <s26288@pjwstk.edu.pl>
2021-11-17 00:00:01 +0100

    Dodanie s03-countdown.cpp i s03-countdown.bin

---
+++ src/s03-countdown.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int sec;
+
+int main()
+{
+	cout << "Number of seconds: \n";
+	cin >> sec;
+	cout << endl;
+	if(cin.fail())
+	{	
+		cout << "That's not a number!\n";
+	}
+
+	while(sec)
+	{
+		for(int i=sec; i >= 0; i--)
+		{
+			cout << i;
+			cout << "...\n";
+			sleep(1);
+			sec=0;
+		}
+		cout << "Done\n";
+	}
+	return 0;
+}
