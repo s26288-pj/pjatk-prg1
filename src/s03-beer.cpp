e1e7bfb1e45efa8e511f510f58a5e31cf56d4dce
s26288 <s26288@pjwstk.edu.pl>
2021-11-19 17:53:21 +0100

    Ćwiczenie numer 3 - zadanie 6;

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-using namespace std;
 
 int beers;
 
 int main()
 {
-	cout << "Enter a number of beers: ";
-	cin >> beers;
-	if(cin.fail())
+	std::cout << "Enter a number of beers: ";
+	std::cin >> beers;
+	if(std::cin.fail())
 	{
-		cout << "That's not a number!\n";
+		std::cout << "That's not a number!\n";
 	}
 	
 	int beers_in = beers;
@@ -17,16 +16,16 @@ int main()
 	for(int i=beers; i >= 1; i--)
 	{		
 		beers--;
-		cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
-		cout << "Take one down, pass it around, ";
+		std::cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
+		std::cout << "Take one down, pass it around, ";
 		if(beers == 0)
 		{
-			cout << "no more bottles of beer on the wall, no more bottles of beer.\n";
-			cout << "Go to the store and buy some more, " << beers_in << " bottles of beer on the wall...\n";
+			std::cout << "no more bottles of beer on the wall, no more bottles of beer.\n";
+			std::cout << "Go to the store and buy some more, " << beers_in << " bottles of beer on the wall...\n";
 		}
 		else
 		{
-			cout << beers << " bottles of beer on the wall...\n\n";
+			std::cout << beers << " bottles of beer on the wall...\n\n";
 		}
 	}
 	return 0;
